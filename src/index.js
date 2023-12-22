5e1148152cdaf459cf69b42fcae284433a7fcfcc
nicknamed <nicknamed19@gmail.com>
2023-12-22 11:23:59 -0500

    Update html & js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const app = document.getElementById('app')
+app.className = 'grid gap-2 p-4 grid-cols-2 justify-items-center2'
 
-console.log('Happy hacking :)')
+const api = 'https://api.thecatapi.com/v1/images/search?limit=1'
+
+const btnCreateImg = document.createElement('button')
+btnCreateImg.className = 'w-32 h-10 bg-green-300 rounded-xl'
+btnCreateImg.textContent = 'Generar imagen'
+
+app.append(btnCreateImg)
+
+btnCreateImg.addEventListener('click', createImg)
+
+function createImg() {
+
+        const figure = document.createElement('figure')
+        figure.className = 'w-40 h-40'
+        
+        const image = document.createElement('img')
+        
+        figure.append(image)
+
+        const getData = async (url) => {
+            const res = await fetch(url)
+            const data = await res.json()
+            image.src = data[0].url
+        }
+        
+        getData(api)
+        app.append(figure)        
+}
+
